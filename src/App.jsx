8546cdcc0e40ebf211fab1be12860d3981cf0267
IRAVISAHU <r8936033768@gmail.com>
2024-08-12 14:19:01 +0530

    hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 
 import "./sass/style.css";
 import Dashboard from "./Dashboard";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Login from "./Login";
 import Patient from "./Patient";
 import PaymentSucess from "./PaymentSucess";
 
 
-
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Dashboard />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/Patient" element={<Patient />}></Route>
         <Route path="/payment-success" element={<PaymentSucess />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 }
